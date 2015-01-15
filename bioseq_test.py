fa32ba27ed7654d74a89ddd8b4cdd431a886b6bd
peritob <peri.tobias@gmail.com>
2015-01-15 21:42:12 +1100

    file name change

--- bioseq_test.py
+++ bioseq_test.py
@@ -2,7 +2,7 @@ from Bio import SeqIO
 with open ("reduced_list_testGH18.txt") as f:
 	required_ids = f.read().splitlines()
 	
-with open("out.fasta","w") as output_handle:
+with open("output.fasta","w") as output_handle:
 	with open("testGH18.fasta", "rU") as handle:
 		for record in SeqIO.parse(handle, "fasta"):
 			if record.id in required_ids:
